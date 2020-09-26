0085fce7867d1633da401abf899d928bdc192f79
0razzu <0razzu@protonmail.com>
2020-09-27 02:48:52 +0600

    Server

--- src/main/java/music_storage/Server.java
+++ src/main/java/music_storage/Server.java
@@ -21,4 +21,59 @@ public class Server {
     public static void stop() throws SQLException {
         DatabaseConnection.closeConnection();
     }
+    
+    
+    public static String addMusician(String musicianJson) {
+        return musicianService.addMusician(musicianJson);
+    }
+    
+    
+    public static String getMusician(String idJson) {
+        return musicianService.getMusician(idJson);
+    }
+    
+    
+    public static String delMusician(String idJson) {
+        return musicianService.delMusician(idJson);
+    }
+    
+    
+    public static String delAllMusicians(String emptyJson) {
+        return musicianService.delAllMusicians(emptyJson);
+    }
+    
+    
+    public static String getMusicianTracks(String idJson) {
+        return musicianService.getMusicianTracks(idJson);
+    }
+    
+    
+    public static String addTrack(String trackJson) {
+        return trackService.addTrack(trackJson);
+    }
+    
+    
+    public static String getTrack(String idJson) {
+        return trackService.getTrack(idJson);
+    }
+    
+    
+    public static String delTrack(String idJson) {
+        return trackService.delTrack(idJson);
+    }
+    
+    
+    public static String delAllTracks(String emptyJson) {
+        return trackService.delAllTracks(emptyJson);
+    }
+    
+    
+    public static String associateMusiciansWithTrack(String trackIdMusicianIdsJson) {
+        return trackService.associateMusiciansWithTrack(trackIdMusicianIdsJson);
+    }
+    
+    
+    public static String getTrackMusicians(String idJson) {
+        return trackService.getTrackMusicians(idJson);
+    }
 }
